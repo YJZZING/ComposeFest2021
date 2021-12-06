3d783ac734eae5c30b86ffe80d37a067fcae7a44
YJZZING <yjzzing@gmail.com>
2021-12-07 00:12:34 +0900

    Layout in Jetpack Compose 7-9

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
@@ -16,16 +16,18 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Print
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
 import com.codelab.layoutscodelab.ui.theme.LayoutsCodelabTheme
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +35,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                //LayoutsCodeLab()
+                LayoutsCodeLab()
                 //SimpleList()
                 //LazyList()
-                ScrollingList()
+                //ScrollingList()
             }
         }
     }
@@ -45,46 +47,91 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LayoutsCodeLab() {
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = " LayoutCodeLab")
-                },
-                actions = {
-                    IconButton(onClick = { /*do something()*/ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-                    }
-                    IconButton(onClick = { /*do something()*/ }) {
-                        Icon(Icons.Filled.Print, contentDescription = null)
-                    }
-                })
-        },
-        bottomBar = {
-            BottomNavigation(
-                backgroundColor = MaterialTheme.colors.primary
-            ) {
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = { /*TODO*/ },
-                    icon = { Icon(Icons.Filled.Home, contentDescription = null) })
-            }
+            topBar = {
+                TopAppBar(
+                        title = {
+                            Text(text = " LayoutCodeLab")
+                        },
+                        actions = {
+                            IconButton(onClick = { /*do something()*/ }) {
+                                Icon(Icons.Filled.Favorite, contentDescription = null)
+                            }
+                            IconButton(onClick = { /*do something()*/ }) {
+                                Icon(Icons.Filled.Print, contentDescription = null)
+                            }
+                        })
+            },
+            bottomBar = {
+                BottomNavigation(
+                        backgroundColor = MaterialTheme.colors.primary
+                ) {
+                    BottomNavigationItem(
+                            selected = true,
+                            onClick = { /*TODO*/ },
+                            icon = { Icon(Icons.Filled.Home, contentDescription = null) })
+                }
 
-        }
+            }
     ) { innerPadding ->
         BodyContent(
-            Modifier
-                .padding(innerPadding)
-                .padding(8.dp)
+                Modifier.padding(innerPadding)
         )
     }
 }
 
+val topics = listOf(
+        "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+        "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+        "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
 @Composable
-private fun BodyContent(modifier: Modifier) {
-    Column(modifier = modifier) {
+private fun BodyContent(modifier: Modifier = Modifier) {
+/*    Column(modifier = modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codeLab")
-    }
+    }*/
+/*    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }*/
+/*    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier, rows = 5) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }*/
+/*    Row(
+            modifier = modifier
+                    .background(color = Color.LightGray)
+                    .size(200.dp)
+                    .padding(16.dp)
+                    .horizontalScroll(rememberScrollState())
+    ) {
+        StaggeredGrid {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }*/
+    Row(
+            modifier = modifier
+                    .background(color = Color.LightGray)
+                    .padding(16.dp)
+                    .size(200.dp)
+                    .horizontalScroll(rememberScrollState()),
+            content =
+            {
+                StaggeredGrid {
+                    for (topic in topics) {
+                        Chip(modifier = Modifier.padding(8.dp), text = topic)
+                    }
+                }
+            })
+
 }
 
 @Preview
@@ -95,28 +142,27 @@ fun LayoutsCodeLabPreview() {
     }
 }
 
-
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
     Row(
-        modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.surface)
-            .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp)
+            modifier
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(MaterialTheme.colors.surface)
+                    .clickable(onClick = { /* Ignoring onClick */ })
+                    .padding(16.dp)
     ) {
         Surface(
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+                modifier = Modifier.size(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
         ) {
             // Image goes here
         }
         Column(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
+                modifier = Modifier
+                        .padding(start = 8.dp)
+                        .align(Alignment.CenterVertically)
         ) {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
@@ -158,9 +204,9 @@ fun LazyList() {
 fun ImageListItem(index: Int) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
-            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
+                painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+                contentDescription = "Android Logo",
+                modifier = Modifier.size(50.dp)
         )
         Spacer(Modifier.width(10.dp))
         Text("Item #$index", style = MaterialTheme.typography.subtitle1)
@@ -171,11 +217,11 @@ fun ImageListItem(index: Int) {
 fun ScrollingList() {
     val listSize = 100
     val scrollState = rememberLazyListState()
-    val  coroutineScope = rememberCoroutineScope()
+    val coroutineScope = rememberCoroutineScope()
     Column {
         Row {
             Button(onClick = {
-                coroutineScope.launch  {
+                coroutineScope.launch {
                     scrollState.animateScrollToItem(0)
                 }
             }) {
@@ -183,8 +229,8 @@ fun ScrollingList() {
             }
 
             Button(onClick = {
-                coroutineScope.launch  {
-                    scrollState.animateScrollToItem(listSize-1)
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
                 }
             }) {
                 Text("Scroll to the end")
@@ -193,6 +239,183 @@ fun ScrollingList() {
         LazyColumn(state = scrollState) {
             items(listSize) {
                 ImageListItem(it)
+            }
+        }
+    }
+}
+
+
+fun Modifier.firstBaselineToTop(
+        firstBaselineToTop: Dp
+) = this.then(layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseLine = placeable[FirstBaseline]
+
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseLine
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        placeable.placeRelative(0, placeableY)
+    }
+}
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+        modifier: Modifier = Modifier,
+        content: @Composable () -> Unit
+) {
+    Layout(
+            modifier = modifier,
+            content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+        modifier: Modifier = Modifier,
+        rows: Int = 3,
+        content: @Composable () -> Unit
+) {
+    Layout(
+            modifier = modifier,
+            content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+
+        val height = rowHeights.sumOf { it }
+                .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                        x = rowX[row],
+                        y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+            modifier = modifier,
+            border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+            shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+                modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                    modifier = Modifier
+                            .size(16.dp, 16.dp)
+                            .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsCodelabTheme {
+        Chip(text = "Hi there")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodeLabPreview2() {
+    LayoutsCodelabTheme {
+        BodyContent()
+    }
+}
+
+@Stable
+fun Modifier.padding(all: Dp) =
+        this.then(
+                PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
+        )
+
+private class PaddingModifier(
+        val start: Dp = 0.dp,
+        val top: Dp = 0.dp,
+        val end: Dp = 0.dp,
+        val bottom: Dp = 0.dp,
+        val rtlAware: Boolean,
+) : LayoutModifier {
+
+    override fun MeasureScope.measure(
+            measurable: Measurable,
+            constraints: Constraints
+    ): MeasureResult {
+
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+        return layout(width, height) {
+            if (rtlAware) {
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            } else {
+                placeable.place(start.roundToPx(), top.roundToPx())
             }
         }
     }
